5aca3dd5af81488b68cbd68930f5d3fb587d8c55
souris-dev <suorees113@gmail.com>
2021-11-28 07:55:17 +0530

    Added missing require for node-fetch

--- services/explore/main.js
+++ services/explore/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const fetch = require("node-fetch");
 
 const dotenv = require("dotenv");
 const { verifyJwt } = require("./utils/jwt_utils");
